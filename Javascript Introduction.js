73c98309bacfffe98da9409502986de44004465c
JabinJohnson <133173143+JabinJohnson@users.noreply.github.com>
2023-06-18 15:49:13 -0400

    Add files via upload

---
+++ Javascript Introduction.js
@@ -0,0 +1,9 @@
+var firstName = Jabin;
+
+var lastName = Johnson;
+
+var age = 21;
+alert("My Name is " + firstName + lastName);
+
+alert("I am " + age + " years old");
+console.log("I am having as much as one can, learning Javascript for the first time.");
